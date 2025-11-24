7604dad46f3669c3cddd7898e5cf88402d60e0c4
Álvaro <62889807+alvarogfn@users.noreply.github.com>
2025-11-24 20:47:11 -0300

    feat: add all dependencies as external dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,25 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+import { dependencies } from "./package.json";
+
+// eslint-disable-next-line unicorn/no-array-reduce
+const paths = Object.entries(dependencies).reduce<Record<string, string>>(
+  (acc, [name, version]) => {
+    acc[name] = `https://esm.sh/${name}@${version}`;
+    return acc;
+  },
+  {},
+);
+
 // https://vite.dev/config/
 export default defineConfig({
   base: "/cronometrando",
   build: {
     rollupOptions: {
+      external: Object.keys(dependencies),
       output: {
-        manualChunks: {
-          fluentui: ["@fluentui/react-components", "@fluentui/react-icons"],
-        },
+        paths,
       },
     },
   },
